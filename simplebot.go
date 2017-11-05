cecc9a08ed60173c4b6f85fbd5ce4e0a07a77ada
Snorri Sturluson <snorri.sturluson@ccpgames.com>
2017-11-05 17:03:54 +0000

    Multiple connections.

--- simplebot.go
+++ simplebot.go
@@ -3,33 +3,82 @@ package main
 import (
 	"net"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"math/rand"
+	"time"
 )
 
-func main() {
-	conn, err := net.Dial("tcp", "localhost:4040")
+type Vector3 struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+type SimpleBot struct {
+	userid int64
+	connection net.Conn
+	targetLocation Vector3
+}
+
+func NewSimpleBot(userid int64) (*SimpleBot) {
+	return &SimpleBot{
+		userid: userid,
+	}
+}
+
+func (bot* SimpleBot) Connect(address string) {
+	conn, err := net.Dial("tcp", address)
 	if err != nil {
-		fmt.Errorf("Couldn't connect")
+		fmt.Printf("Couldn't connect to %v", address)
 		return
 	}
+	fmt.Printf("Connection established\n")
+	bot.connection = conn
 
-	fmt.Printf("Connection established")
+	msg := fmt.Sprintf(`{"user": %d}` + "\n", bot.userid)
+	bot.connection.Write([]byte(msg))
+}
 
-	msg := "{\"user\": 0}\n"
-	_, err = conn.Write([]byte(msg))
+func (bot* SimpleBot) SetTargetLocation(loc Vector3) {
+	msg := fmt.Sprintf(`{"settargetlocation": {"x": %v, "y": %v, "z": %v}}` + "\n", loc.X, loc.Y, loc.Z)
+	bot.connection.Write([]byte(msg))
 
-	if err != nil {
-		fmt.Errorf("Couldn't write")
-		return
-	}
+}
 
+func (bot* SimpleBot) ReceiveLoop() {
 	for {
 		recvBuf := make([]byte, 4096)
-		n, err := conn.Read(recvBuf)
+		_, err := bot.connection.Read(recvBuf)
 		if err != nil {
-			fmt.Print(err)
+			fmt.Printf("Error in Read: %v\n", err)
 			break
 		}
-		msgReceived := string(recvBuf[:n])
-		fmt.Printf("Message Received: %s\n", msgReceived)
 	}
+}
+
+func (bot* SimpleBot) BehaviorLoop() {
+	for bot.connection != nil {
+		bot.SetTargetLocation(Vector3{X: rand.Float64() * 800, Y: rand.Float64() * 600, Z: 0.0})
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func main() {
+	for i:= 0; i < 100; i++ {
+		bot := NewSimpleBot(int64(i))
+		bot.Connect("localhost:4040")
+		if bot.connection == nil {
+			return
+		}
+		go bot.ReceiveLoop()
+		go bot.BehaviorLoop()
+
+		time.Sleep(1 * time.Second)
+	}
+
+	exitSignal := make(chan os.Signal)
+	signal.Notify(exitSignal, syscall.SIGINT, syscall.SIGTERM)
+	<-exitSignal
 }
